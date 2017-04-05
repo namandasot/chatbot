9604bdce8139edf8d4d7d988d6363d64343cf91e
naman.dasot@hdfcred.com <naman.dasot@hdfcred.com>
2017-04-05 18:41:55 +0530

    asd

--- apiPlain.py
+++ apiPlain.py
@@ -2,14 +2,16 @@
 from flask import Flask,request,jsonify
 from gevent.wsgi import WSGIServer
 
+@app.route('/')
+def get():
+	return jsonify('Hello, World!')
 
+	
 if __name__ == '__main__':
 	http_server = WSGIServer(('0.0.0.0', 5050), app)
     http_server.serve_forever()
 
 
-@app.route('/')
-def get():
-	return jsonify('Hello, World!')
+
 
 
