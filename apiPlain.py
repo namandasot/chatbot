5c08fe1d8899b4e12ac0fb12c3836692ace077ba
naman.dasot@hdfcred.com <naman.dasot@hdfcred.com>
2017-04-05 19:10:14 +0530

    asd

--- apiPlain.py
+++ apiPlain.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 from flask import Flask,request,jsonify
 from gevent.wsgi import WSGIServer
+from os import environ
+# environ.get('PORT')
 app = Flask(__name__)
-
 @app.route('/')
 def get():
 	return jsonify('Hello, World!')
 
 	
 if __name__ == '__main__':
-	http_server = WSGIServer(('0.0.0.0', 5050), app)
-	http_server.serve_forever()
+	app.run(environ.get('PORT'))
+	# http_server = WSGIServer(('0.0.0.0', 5050), app)
+	# http_server.serve_forever()
 
 
 
